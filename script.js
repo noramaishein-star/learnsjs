302487347b46f0dd58f9f41c8332436e870d6513
Nora Mai Shein <noramai.shein@gmail.com>
2025-09-30 09:49:24 +0300

    add Maths example

--- script.js
+++ script.js
@@ -1,37 +1,30 @@
-console.log('Hello World!');
-var num = 1; // don't use var anymore
-let num2 = 2; 
-console.log(num2, num);
-num = 3.5;
-console.log (typeof num);
+console.log(1 + 2);
+let num = 1;
+console.log(num);
+num = num + 2;
+console.log(num);
+num = num - 1;
+console.log(num);
+num = num * 2;
+console.log(num);
+num = num / 3;
+console.log(num);
 num = 3;
-console.log (typeof num);
-let text = 'NoraMai';
-console.log (typeof text);
-let bool = true;
-console.log (typeof bool);
-bool = false;
-console.log (typeof bool);
-let nothing;
-console.log (typeof nothing);
-nothing = undefined;
-nothing = null;
-console.log (typeof nothing);
-let array = [1, 2, 3, 4, 'sad', true, 3.5];
-console.log (typeof array);
-let obj = {
-    name: 'NoraMai',
-    age: 17,
-    cats: ['Miinu', 'Liisu', 'Alfres'],
-    data: {
-        phone: '123456789'
-        email: 'ilove.my.mom'
-    }
-}
-console.log (typeof obj);
-
-function hello () {
-    console.log('hello');
-}
-
-console.log (typeof hello);
+num = num ** 3;
+console.log(num);
+num = 9;
+num = num % 2; // modulo
+console.log(num);
+num +=2;
+num -=2;
+num *=2;
+num /=2;
+num **=2;
+num %=2;
+console.log(num);
+num++;
+console.log(num);
+num--;
+console.log(num);
+num = Math.sqrt(9);
+console.log(num);
