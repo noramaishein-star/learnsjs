682dda46a1e30b45b57203e1f93ea5bb6eb83716
Nora Mai Shein <noramai.shein@gmail.com>
2025-10-21 09:37:18 +0300

    Add examples

--- script.js
+++ script.js
@@ -1,72 +1,18 @@
-function hello() {
-    console.log('Hello World!');
-}
+let nums = [1, 2, 3, 4];
+let moreNums = [5, 6, ...nums, 7, 8];
+console.log(moreNums);
 
-hello();
-hello();
-hello();
-
-function helloName(name='Nameless') {
-    console.log(`Hello ${name}!`);
-}
-
-helloName('Nora');
-helloName('Mai');
-helloName('Sein');
-helloName();
-
-function sum(a, b) {
-    return a+b;
-    console.log('After return'); // never runs
-}
-
-let answer = sum(4, 6);
-console.log(answer);
-
-let sum2 = function (a,b) {
-    return a+b
-}
-console.log(sum2(3,5));
-
-let sum3 = (a,b) => {
-    return a+b;
-}
-
-console.log(sum3(2,3));
-
-let sum4 = (a,b) => a+b;
-
-console.log(sum4(6,4));
-
-let sqeare = a => a*a;
-
-console.log(square(6));
-
-let obj = {
-    name: 'Nora'
-    age: 17;
-    sum1: function (a,b) {
-        return a+b+this.age;
-    },
-    sum2: (a,b) => {
-        return a+b+this.age;
-    },
-    sum2:(a,b) => a+b+this.age,
-    sum4(a,b) {
-        return a+b+this.age;
+function sum(...args) {
+    console.log(args);
+    let sum =0
+    for(let i=0; i<args.length; i++) {
+        sum+=args[i];
     }
+    return sum;
 }
 
-console.log(obj.sum1(1,1)):
-console.log(obj.sum2(1,1));
-console.log(obj.sum3(1,1));
-console.log(obj.sum4(1,1));
-
-function recursive(i){
-    if(i<10){
-        console.log(i);
-        recursive(i+1);
-    }
-}
-
-recursive(0);
+sum(1,2);
+let a = sum(1,2,3,4,5,6,7,8,9);
+console.log(a);
+a = sum(...moreNums);
+console.log(a);
