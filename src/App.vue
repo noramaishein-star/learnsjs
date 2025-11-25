36f860f1a02e38420c22947a4f467190e72d0ffd
Nora Mai Shein <noramai.shein@gmail.com>
2025-11-25 09:59:36 +0200

    Todo list

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,64 @@
 <script setup>
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 
- 
+
 let message = ref('Hello Vue');
 let isDanger = ref(false);
+let newItem = ref('Piim')
+let i = 0;
+let items = ref([
+  { id: i++, name: 'Piim', isDone: true },
+  { id: i++, name: 'Su ema', isDone: false },
+  { id: i++, name: 'Veel piima', isDone: false },
+])
+
+function add() {
+  if (newItem.value.trim() !== '') {
+    items.value.push({ id: i++, name: newItem.value, isDone: false });
+  }
+  newItem.value = '';
+}
+
+let doneItems = computed(() => items.value.filter(item => item.isDone));
+
+
 </script>
 
 <template>
+  <div class="container mt-2">
     <div class="content">
-        <button class="button is-primary" @click="message = 'Hello VM24'">Click me!</button>
-         <h1>{{ message }}</h1>
-         <button class="button" :class="{'is-danger': isDanger, 'is-warning': !isDanger}" @click="isDanger = !isDanger">Cool button</button>
+      <button class="button is-primary" @click="message = 'Hello VM24'">Click me!</button>
+      <h1>{{ message }}</h1>
+      <button class="button" :class="{ 'is-danger': isDanger, 'is-warning': !isDanger }"
+        @click="isDanger = !isDanger">Cool button</button>
+
+      <div class="field has-addons mt-2">
+        <div class="control is-expanded">
+          <input v-model="newItem" @keydown.enter="add" class="input" type="text" placeholder="Add item">
+        </div>
+        <div class="control">
+          <button class="button is-info" @click="items.push(newItem)">
+            Add item
+          </button>
+        </div>
+      </div>
+      <h1>{{ newItem.split('').reverse().join('') }}</h1>
+
+      <h1>All Items</h1>
+      <ul>
+        <li v-for="item in items">
+          {{ item.name }}
+          <input type="checkbox" v-model="item.isDone">
+        </li>
+      </ul>
+
+      <h1>Done Items</h1>
+      <ul>
+        <li v-for="item in doneItems">
+          {{ item.name }}
+          <input type="checkbox" v-model="item.isDone">
+        </li>
+      </ul>
     </div>
+  </div>
 </template>
