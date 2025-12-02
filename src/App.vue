97281795ad5fc19da6333b01a88b603009900bbb
Nora Mai Shein <noramai.shein@gmail.com>
2025-12-02 09:57:26 +0200

    Add Modal example

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,21 @@
 <script setup>
-import { computed, ref } from 'vue';
-import ItemList from './ItemList.vue';
-
-let message = ref('Hello Vue');
-let isDanger = ref(false);
-let newItem = ref('Piim')
-let i = 0;
-let items = ref([
-  { id: i++, name: 'Piim', isDone: true },
-  { id: i++, name: 'Su ema', isDone: false },
-  { id: i++, name: 'Veel piima', isDone: false },
-])
-
-function add() {
-  if (newItem.value.trim() !== '') {
-    items.value.push({ id: i++, name: newItem.value, isDone: false });
-  }
-  newItem.value = '';
-}
-
-let doneItems = computed(() => items.value.filter(item => item.isDone));
-let toDoItems = computed(() => items.value.filter(item => !item.isDone));
+import { ref } from 'vue';
+import Modal from './components/Modal.vue';
 
 
+let modal1Active = ref(false);
+let modal2Active = ref(false);
 
 </script>
 
 <template>
   <div class="container mt-2">
     <div class="content">
-      <button class="button is-primary" @click="message = 'Hello VM24'">Click me!</button>
-      <h1>{{ message }}</h1>
-      <button class="button" :class="{ 'is-danger': isDanger, 'is-warning': !isDanger }"
-        @click="isDanger = !isDanger">Cool button</button>
-
-      <div class="field has-addons mt-2">
-        <div class="control is-expanded">
-          <input v-model="newItem" @keydown.enter="add" class="input" type="text" placeholder="Add item">
-        </div>
-        <div class="control">
-          <button class="button is-info" @click="items.push(newItem)">
-            Add item
-          </button>
-        </div>
-      </div>
-      <h1>{{ newItem.split('').reverse().join('') }}</h1>
-
-      <ItemList :items="items" title="All Items"></ItemList>
-      <ItemList :items="doneItems" title="Done Items"></ItemList>
-      <ItemList :items="toDoItems" title="toDo Items"></ItemList>
+      <button class="button is-primary" @click="modal1Active = true">Open Modal 1</button>
+      <button class="button is-link" @click="modal2Active = true">Open Modal 2</button>
+      
+      <modal :active="modal1Active" @close="modal1Active = false" url="https://picsum.photos/1280/960?random=1"></modal>
+      <modal :active="modal2Active" @close="modal2Active = false" url="https://picsum.photos/1280/960?random=2"></modal>
     </div>
   </div>
 </template>
---
+++ src/pages/ToDo.vue
@@ -0,0 +1,54 @@
+<script setup>
+import { computed, ref } from 'vue';
+import ItemList from './components/ItemList.vue';
+
+let message = ref('Hello Vue');
+let isDanger = ref(false);
+let newItem = ref('Piim')
+let i = 0;
+let items = ref([
+  { id: i++, name: 'Piim', isDone: true },
+  { id: i++, name: 'Su ema', isDone: false },
+  { id: i++, name: 'Veel piima', isDone: false },
+])
+
+function add() {
+  if (newItem.value.trim() !== '') {
+    items.value.push({ id: i++, name: newItem.value, isDone: false });
+  }
+  newItem.value = '';
+}
+
+let doneItems = computed(() => items.value.filter(item => item.isDone));
+let toDoItems = computed(() => items.value.filter(item => !item.isDone));
+
+
+
+</script>
+
+<template>
+  <div class="container mt-2">
+    <div class="content">
+      <button class="button is-primary" @click="message = 'Hello VM24'">Click me!</button>
+      <h1>{{ message }}</h1>
+      <button class="button" :class="{ 'is-danger': isDanger, 'is-warning': !isDanger }"
+        @click="isDanger = !isDanger">Cool button</button>
+
+      <div class="field has-addons mt-2">
+        <div class="control is-expanded">
+          <input v-model="newItem" @keydown.enter="add" class="input" type="text" placeholder="Add item">
+        </div>
+        <div class="control">
+          <button class="button is-info" @click="items.push(newItem)">
+            Add item
+          </button>
+        </div>
+      </div>
+      <h1>{{ newItem.split('').reverse().join('') }}</h1>
+
+      <ItemList :items="items" title="All Items"></ItemList>
+      <ItemList :items="doneItems" title="Done Items"></ItemList>
+      <ItemList :items="toDoItems" title="toDo Items"></ItemList>
+    </div>
+  </div>
+</template>
