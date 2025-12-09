5dc7b3b336082c6eb2df1b37f362889868370fd0
Nora Mai Shein <noramai.shein@gmail.com>
2025-12-09 09:34:21 +0200

    Add tabs component and examples

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,26 @@
 <script setup>
 import { ref } from 'vue';
-import Modal from './components/Modal.vue';
+import Tab from './components/Tab.vue';
+let activeTab = ref(0);
+let titles = ref(['Pictures', 'Music', 'Videos', 'Documents', 'Games'])
+let contents = ref([
+  'Some awesome Pictures',
+  'Some awesome Videos',
+  'Some awesome Music',
+  'Some awesome Documents',
+  'Some awesome Games',
 
 
-let modal1Active = ref(false);
-let modal2Active = ref(false);
-let modal3Active = ref(false);
+])
 
+function setTab(index){
+  activeTab.value = index;
+}
 </script>
 
 <template>
-  <div class="container mt-2">
-    <div class="content">
-      <button class="button is-primary" @click="modal1Active = true">Open Modal 1</button>
-      <button class="button is-link" @click="modal2Active = true">Open Modal 2</button>
-      <button class="button is-warning" @click="modal3Active = true">Open Modal 3</button>
-
-      
-      <Modal :active="modal1Active" @close="modal1Active = false" url="https://picsum.photos/1280/960?random=1"></Modal>
-      <Modal :active="modal2Active" @close="modal2Active = false" url="https://picsum.photos/1280/960?random=2"></Modal>
-      <Modal :active="modal3Active" @close="modal3Active = false">
-<div class="card">
-  <div class="card-content">
-    <div class="content">
-      Lorem ipsum leo risus, porta ac consectetur ac, vestibulum at eros. Donec
-      id elit non mi porta gravida at eget metus. Cum sociis natoque penatibus
-      et magnis dis parturient montes, nascetur ridiculus mus. Cras mattis
-      consectetur purus sit amet fermentum.
+    <Tab :active="activeTab" :titles="titles" @change="setTab"></Tab>
+    <div class="container content">
+    <h1>{{ contents[activeTab] }}</h1>
     </div>
-  </div>
-</div>
-      </Modal>
-    </div>
-  </div>
 </template>
---
+++ src/pages/Modals.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { ref } from 'vue';
+import Modal from '../components/Modal.vue';
+
+
+let modal1Active = ref(false);
+let modal2Active = ref(false);
+let modal3Active = ref(false);
+
+</script>
+
+<template>
+  <div class="container mt-2">
+    <div class="content">
+      <button class="button is-primary" @click="modal1Active = true">Open Modal 1</button>
+      <button class="button is-link" @click="modal2Active = true">Open Modal 2</button>
+      <button class="button is-warning" @click="modal3Active = true">Open Modal 3</button>
+
+      
+      <Modal :active="modal1Active" @close="modal1Active = false" url="https://picsum.photos/1280/960?random=1"></Modal>
+      <Modal :active="modal2Active" @close="modal2Active = false" url="https://picsum.photos/1280/960?random=2"></Modal>
+      <Modal :active="modal3Active" @close="modal3Active = false">
+<div class="card">
+  <div class="card-content">
+    <div class="content">
+      Lorem ipsum leo risus, porta ac consectetur ac, vestibulum at eros. Donec
+      id elit non mi porta gravida at eget metus. Cum sociis natoque penatibus
+      et magnis dis parturient montes, nascetur ridiculus mus. Cras mattis
+      consectetur purus sit amet fermentum.
+    </div>
+  </div>
+</div>
+      </Modal>
+    </div>
+  </div>
+</template>
