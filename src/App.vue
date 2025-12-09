6cc6de613382b6bf9995c64f88af4801447317b7
Nora Mai Shein <noramai.shein@gmail.com>
2025-12-09 08:42:58 +0200

    Add slot examples

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@ import Modal from './components/Modal.vue';
 
 let modal1Active = ref(false);
 let modal2Active = ref(false);
+let modal3Active = ref(false);
 
 </script>
 
@@ -13,9 +14,23 @@ let modal2Active = ref(false);
     <div class="content">
       <button class="button is-primary" @click="modal1Active = true">Open Modal 1</button>
       <button class="button is-link" @click="modal2Active = true">Open Modal 2</button>
+      <button class="button is-warning" @click="modal3Active = true">Open Modal 3</button>
+
       
-      <modal :active="modal1Active" @close="modal1Active = false" url="https://picsum.photos/1280/960?random=1"></modal>
-      <modal :active="modal2Active" @close="modal2Active = false" url="https://picsum.photos/1280/960?random=2"></modal>
+      <Modal :active="modal1Active" @close="modal1Active = false" url="https://picsum.photos/1280/960?random=1"></Modal>
+      <Modal :active="modal2Active" @close="modal2Active = false" url="https://picsum.photos/1280/960?random=2"></Modal>
+      <Modal :active="modal3Active" @close="modal3Active = false">
+<div class="card">
+  <div class="card-content">
+    <div class="content">
+      Lorem ipsum leo risus, porta ac consectetur ac, vestibulum at eros. Donec
+      id elit non mi porta gravida at eget metus. Cum sociis natoque penatibus
+      et magnis dis parturient montes, nascetur ridiculus mus. Cras mattis
+      consectetur purus sit amet fermentum.
+    </div>
+  </div>
+</div>
+      </Modal>
     </div>
   </div>
 </template>
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -5,9 +5,10 @@ defineProps(['active', 'url'])
      <div class="modal" :class="{ 'is-active': active }">
         <div class="modal-background" @click="$emit('close')"></div>
         <div class="modal-content">
-          <p class="image is-4by3">
+          <p v-if="url" class="image is-4by3">
             <img :src="url" alt="">
           </p>
+          <slot v-else></slot>
         </div>
         <button class="modal-close is-large" aria-label="close" @click="$emit('close')"></button>
       </div>
