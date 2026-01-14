57138e9089c58746bd959bb3b3e76159ce0e669b
Nora Mai Shein <noramai.shein@gmail.com>
2026-01-14 11:05:08 +0200

    Add active link to RouterTab

--- src/components/RouterTab.vue
+++ src/components/RouterTab.vue
@@ -5,8 +5,12 @@
 <template>
  <div class="tabs is-centered">
   <ul>
-    <li><RouterLink to="/">ToDo</RouterLink></li>
-    <li><RouterLink to="/modals">Modals</RouterLink></li>
+    <li :class="{'is-active': $route.path === '/'}">
+        <RouterLink to="/">ToDo</RouterLink>
+    </li>
+    <li :class="{'is-active': $route.path === '/modals'}">
+        <RouterLink to="/modals">Modals</RouterLink>
+    </li>
   </ul>
 </div>
 </template>
