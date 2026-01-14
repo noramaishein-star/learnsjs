f79c9825974848e07f20251646dedf6c9e44b7f7
Nora Mai Shein <noramai.shein@gmail.com>
2026-01-14 10:56:48 +0200

    Add vue router

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,8 @@
 <script setup>
-import { ref } from 'vue'; 
-import Tab from './components/Tab.vue';
-
-import ToDo from './pages/ToDo.vue';
-import Modals from './pages/Modals.vue';
-
-let activeTab = ref(0);
-let titles = ref(['ToDo', 'Modals']);
-let contents = ref([
-    ToDo,
-    Modals,
-]);
-
-function setTab(index){
-  activeTab.value = index;
-}
+import RouterTab from './components/RouterTab.vue';
 </script>
 
 <template>
-    <Tab :active="activeTab" :titles="titles" @change="setTab"></Tab>
-    <component :is="contents[activeTab]"></component>
+<RouterTab></RouterTab>
+ <RouterView></RouterView>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,21 @@
 import { createApp } from "vue";
+import { createMemoryHistory, createWebHashHistory, createWebHistory, createRouter } from 'vue-router'
+
+
+import Modals from './pages/Modals.vue';
+import ToDo from './pages/ToDo.vue';
+
+
+const routes = [
+  { path: '/', component: ToDo },
+  { path: '/modals', component: Modals },
+];
+
+export const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
 
 import App from './App.vue';
 
-createApp(App).mount('#app');
+createApp(App).use(router).mount('#app');
